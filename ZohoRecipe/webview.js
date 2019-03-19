962176697b8af535638ccdc76141afb9411de620
Braydie Grove <braydie.grove@gmail.com>
2019-03-19 14:45:31 +0000

    Refactored to ternary if and arrow func

--- ZohoRecipe/webview.js
+++ ZohoRecipe/webview.js
@@ -1,10 +1,1 @@
-module.exports = (Franz) => {
-    function getTimer() {
-        if (document.querySelector('.timer-running') != null){
-            Franz.setBadge(1);     
-        }        else{
-            Franz.setBadge(0);
-        }
-    }
-    Franz.loop(getTimer);
-  }
+module.exports = Franz => Franz.loop(() => Franz.setBadge(document.querySelector('.timer-running') != null ? 1 : 0));
